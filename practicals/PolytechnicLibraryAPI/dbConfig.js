60b957ded6d06c7f6e954ee73e24a6c50149d8a9
LifeRaider <S10257399@connect.np.edu.sg>
2024-06-27 10:52:55 +0800

    Feat : Setup

--- practicals/PolytechnicLibraryAPI/dbConfig.js
+++ practicals/PolytechnicLibraryAPI/dbConfig.js
@@ -1,11 +1,13 @@
+require('dotenv').config()
+
 module.exports = {
-    user: "username", // Replace with your SQL Server login username
-    password: "password", // Replace with your SQL Server login password
-    server: "localhost",
-    database: "bed_db",
-    trustServerCertificate: true,
-    options: {
-      port: 1433, // Default SQL Server port
-      connectionTimeout: 60000, // Connection timeout in milliseconds
-    },
-  };
+  user: process.env.USER, // Replace with your SQL Server login username
+  password: process.env.PASSWORD, // Replace with your SQL Server login password
+  server: process.env.SERVER,
+  database: process.env.DATABASE,
+  trustServerCertificate: true,
+  options: {
+    port: 1433, // Default SQL Server port
+    connectionTimeout: 60000, // Connection timeout in milliseconds
+  },
+};
